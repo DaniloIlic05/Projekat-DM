b607c2c900368601770748a533d242f960e376d0
DaniloIlic05 <123653090+DaniloIlic05@users.noreply.github.com>
2023-05-16 12:43:05 +0200

    Add files via upload

---
+++ Projekat DM/Projekat DM/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Projekat_DM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Одељење";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.textBox1.Location = new System.Drawing.Point(15, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label2.Location = new System.Drawing.Point(131, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Име и презиме";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.textBox2.Location = new System.Drawing.Point(84, 88);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(202, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label3.Location = new System.Drawing.Point(322, 56);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Тип матуре";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Општи",
+            "Стручни",
+            "Уметнички"});
+            this.comboBox1.Location = new System.Drawing.Point(294, 88);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 27);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label4.Location = new System.Drawing.Point(485, 56);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Језик";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Српски језик",
+            "Албански језик",
+            "Босански језик",
+            "Бугарски језик",
+            "Мађарски језик",
+            "Румунски језик",
+            "Русински језик",
+            "Словачки језик",
+            "Хрватски језик"});
+            this.comboBox2.Location = new System.Drawing.Point(441, 88);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(139, 27);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label5.Location = new System.Drawing.Point(638, 56);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Назив првог предмета";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Српски језик и књижевност",
+            "Албански језик и књижевност",
+            "Босански језик и књижевност",
+            "Бугарски језик и књижевност",
+            "Мађарски језик и књижевност",
+            "Румунски језик и књижевност",
+            "Русински језик и књижевност",
+            "Словачки језик и књижевност",
+            "Хрватски језик и књижевност"});
+            this.comboBox3.Location = new System.Drawing.Point(588, 88);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(251, 27);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label6.Location = new System.Drawing.Point(865, 56);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Назив другог предмета";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label7.Location = new System.Drawing.Point(1063, 56);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 19);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Назив трећег предмета";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Математика"});
+            this.comboBox4.Location = new System.Drawing.Point(847, 88);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(181, 27);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(1036, 88);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(214, 27);
+            this.comboBox5.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.button1.Location = new System.Drawing.Point(325, 294);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 124);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Провери и сачувај";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.textBox4.Location = new System.Drawing.Point(19, 212);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(271, 27);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.label8.Location = new System.Drawing.Point(69, 180);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(157, 19);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Фајл за чување и читање";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.button2.Location = new System.Drawing.Point(631, 294);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(298, 124);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Учитај податке";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(15, 136);
+            this.comboBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(1235, 27);
+            this.comboBox6.TabIndex = 19;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(1255, 458);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Segoe Print", 8.25F);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Пројекат";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox6;
+    }
+}
+
---
+++ Projekat DM/Projekat DM/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Projekat_DM
+{
+
+    public partial class Form1 : Form
+    {
+        public int poz = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] temp = {"Биологија", "Географија", "Енглески језик", "Историја", "Италијански језик", "Немачки језик", "Руски језик", "Српски као нематерњи језик", "Физика", "Француски језик", "Хемија", "Шпански језик"};
+            
+            string[] temp1 = {"Зоотехничар", "Техничар за биотехнологију", "Техничар пољопривредне технике", "Техничар хортикултуре", "Техничар за пејзажну архитектуру", "Шумарски техничар", "Геолошки техничар за геотехнику и хидрогеологију", "Геолошки техничар за истраживање минералних сировина", "Рударски техничар", "Рударски техничар за припрему минералних сировина", "Бродомашински техничар", "Машински техничар за компјутерско конструисање", "Машински техничар мерне и регулационе технике", "Машински техничар моторних возила", "Техничар грејања и климатизације", "Техничар за компјутерско управљање(CNC) машина", "Техничар за роботику", "Техничар машинске енергетике", "Техничар оптике", "Електротехничар аутоматике", "Електротехничар електромоторних погона", "Електротехничар електронике", "Електротехничар енергетике", "Електротехничар за термичке и расхладне уређаје", "Електротехничар информационих технологија", "Електротехничар процесног управљања", "Електротехничар рачунара", "Техничар графичке дораде", "Техничар за заштиту животне средине", "Техничар за индустријску фармацеутску технологију", "Техничар штампе", "Фотограф", "Хемијски лаборант", "Хемијско - технолошки техничар", "Текстилни техничар", "Грађевински техничар за лабораторијска испитивања", "Грађевински техничар за хидроградњу", "Извођач инсталатерских и завршних грађевинских радова", "Наутички техничар – речни смер", "Саобраћајно - транспортни техничар", "Техничар вуче", "Техничар ПТТ саобраћаја", "Техничар унутрашњег транспорта", "Транспортни комерцијалиста", "Аранжер у трговини и Трговински техничар", "Кулинарски техничар", "Угоститељски техничар", "Економски техничар", "Финансијски техничар", "Царински техничар", "Гинеколошко - акушерска сестра", "Зубни техничар", "Медицинска сестра – васпитач", "Педијатријска сестра – техничар", "Санитарно - еколошки техничар", "Сценски маскер и власуљар"};
+
+            comboBox4.SelectedIndex = 0;
+            comboBox5.Text = "";
+            switch (comboBox1.Text)
+            {
+                case "Општи":
+                    comboBox5.Items.Clear();
+                    temp.ToList().ForEach(item => comboBox5.Items.Add(item));
+                    break;
+                case "Стручни":
+                    comboBox5.Items.Clear();
+                    temp1.ToList().ForEach(item => comboBox5.Items.Add(item));
+                    break;
+                case "Уметнички":
+                    comboBox5.Items.Clear();
+                    comboBox5.Items.Add("Солфеђо и хармонија");
+                    comboBox5.SelectedIndex = 0;
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox3.Text = "";
+            comboBox3.Text = Convert.ToString(comboBox2.SelectedItem) + " и књижевност";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox6.Text == "")
+            {
+                if(textBox1.Text == "" || textBox2.Text == "" || comboBox1.Text == "" || comboBox2.Text == "" || comboBox3.Text == "" || comboBox4.Text == "" || comboBox5.Text == "")
+                {
+                    MessageBox.Show("Унесите све податке!");
+                }
+                else
+                {
+                    comboBox6.Text = textBox1.Text + ";" + textBox2.Text + ";" + comboBox1.Text + ";" + comboBox2.Text + ";" + comboBox3.Text + ";" + comboBox4.Text + ";" + comboBox5.Text + ";";
+                }
+            }
+            else
+            {
+                if(textBox4.Text == "")
+                {
+                    MessageBox.Show("Унесите име фајла!");
+                }
+                else
+                {
+                    List<string> linije = File.ReadAllLines(textBox4.Text).ToList<string>();
+
+                    if((linije.Count == 0) || (poz == -1))
+                    {
+                        linije.Add(comboBox6.Text);
+                    }
+                    else
+                    {
+                        linije[poz] = comboBox6.Text;
+                    }
+
+                    File.WriteAllLines((textBox4.Text), linije.ToArray());
+
+                    comboBox6.Items.Clear();
+                    linije.ForEach(item => comboBox6.Items.Add(item));
+                }
+
+                comboBox6.Text = "";
+
+                poz = -1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("Унесите име фајла!");
+            }
+            else
+            {
+                List<string> linije = File.ReadAllLines(textBox4.Text).ToList<string>();
+
+                comboBox6.Items.Clear();
+                linije.ForEach(item => comboBox6.Items.Add(item));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] niz = comboBox6.Text.Split(';');
+
+            poz = comboBox6.SelectedIndex;
+
+            for(int i = 0; i < 7; i++)
+            {
+                switch(i)
+                {
+                    case 0:
+                        textBox1.Text = niz[0];
+                        break;
+                    case 1:
+                        textBox2.Text = niz[1];
+                        break;
+                    case 2:
+                        comboBox1.Text = niz[2];
+                        break;
+                    case 3:
+                        comboBox2.Text = niz[3];
+                        break;
+                    case 4:
+                        comboBox3.Text = niz[4];
+                        break;
+                    case 5:
+                        comboBox4.Text = niz[5];
+                        break;
+                    case 6:
+                        comboBox5.Text = niz[6];
+                        break;
+
+                }
+            }
+
+            comboBox6.Text = "";
+            comboBox6.SelectedText = "";
+        }
+    }
+}
